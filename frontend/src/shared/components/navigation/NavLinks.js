aec83e381176ceae2417d46466c6937a80f01c5a
mdshemul48 <mdshemul480@gmail.com>
2021-04-19 20:53:00 +0600

    adding logout btn

--- frontend/src/shared/components/navigation/NavLinks.js
+++ frontend/src/shared/components/navigation/NavLinks.js
@@ -1,32 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 // castom css import
 import "./NavLink.css";
 const NavLinks = () => {
   return (
-    <ul className="menu-bar">
-      <li className="menu-item">
-        <NavLink className="menu-link" exact to="/">
-          Dashboard
-        </NavLink>
-      </li>
-      <li className="menu-item">
-        <NavLink className="menu-link" to="/downloads">
-          Downloads
-        </NavLink>
-      </li>
-      <li className="menu-item">
-        <NavLink className="menu-link" to="/publisher">
-          Publisher
-        </NavLink>
-      </li>
-      <li className="menu-item">
-        <NavLink className="menu-link" to="/create-user">
-          Create User
-        </NavLink>
-      </li>
-    </ul>
+    <React.Fragment>
+      <ul className="menu-bar">
+        <li className="menu-item">
+          <NavLink className="menu-link" exact to="/">
+            Dashboard
+          </NavLink>
+        </li>
+        <li className="menu-item">
+          <NavLink className="menu-link" to="/downloads">
+            Downloads History
+          </NavLink>
+        </li>
+        <li className="menu-item">
+          <NavLink className="menu-link" to="/publisher">
+            Publisher
+          </NavLink>
+        </li>
+        <li className="menu-item">
+          <NavLink className="menu-link" to="/create-user">
+            Create User
+          </NavLink>
+        </li>
+        <li className="menu-item logout">
+          <NavLink to="/logout" exact className="menu-link">
+            <FontAwesomeIcon icon={faSignOutAlt} /> logout
+          </NavLink>
+        </li>
+      </ul>
+    </React.Fragment>
   );
 };
 export default NavLinks;
