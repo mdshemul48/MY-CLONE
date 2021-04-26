14afd2236d160baab21ea0bc357beabf588a06ac
mdshemul48 <mdshemul480@gmail.com>
2021-04-26 22:23:09 +0600

    adding chart data

--- frontend/src/Dashboard/components/StatusChart.js
+++ frontend/src/Dashboard/components/StatusChart.js
@@ -5,29 +5,8 @@ import { Line } from "react-chartjs-2";
 import "./StatusChart.css";
 const StatusChart = (props) => {
   const data = {
-    labels: [
-      "19-04-2021",
-      "20-04-2021",
-      "21-04-2021",
-      "22-04-2021",
-      "23-04-2021",
-      "24-04-2021",
-      "25-04-2021",
-    ],
-    datasets: [
-      {
-        label: "uploaded",
-        data: [33, 53, 85, 41, 44, 65, 75],
-        fill: false,
-        borderColor: "#0074e0",
-      },
-      {
-        label: "Published",
-        data: [53, 12, 43, 64, 76, 12, 85],
-        fill: false,
-        borderColor: "rgb(4, 170, 109,1)",
-      },
-    ],
+    labels: props.labels,
+    datasets: props.datasets,
   };
   return (
     <div className={`StatusChart ${props.className}`}>
