9cc888bdb49e6979aef9f18c9395632eefcfca0b
mdshemul48 <mdshemul480@gmail.com>
2021-04-28 21:31:32 +0600

    adding download history line

--- frontend/src/Download_History/components/DownloadHistory.js
+++ frontend/src/Download_History/components/DownloadHistory.js
@@ -6,7 +6,24 @@ import "./DownloadHistory.css";
 const DonwloadHistory = (props) => {
   return (
     <Card className="download-report">
-      <HistoryLine>01-04-2021 (33 movies)</HistoryLine>
+      <HistoryLine id="34534535345345345345" checked={true}>
+        01-04-2021 (33 movies)
+      </HistoryLine>
+      <HistoryLine id="34534535345345345345" checked={false}>
+        01-04-2021 (33 movies)
+      </HistoryLine>
+      <HistoryLine id="34534535345345345345" checked={true}>
+        01-04-2021 (33 movies)
+      </HistoryLine>
+      <HistoryLine id="34534535345345345345" checked={true}>
+        01-04-2021 (33 movies)
+      </HistoryLine>
+      <HistoryLine id="34534535345345345345" checked={true}>
+        01-04-2021 (33 movies)
+      </HistoryLine>
+      <HistoryLine id="34534535345345345345" checked={false}>
+        01-04-2021 (33 movies)
+      </HistoryLine>
     </Card>
   );
 };
--- frontend/src/Download_History/components/HistoryLine.js
+++ frontend/src/Download_History/components/HistoryLine.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import "./HistoryLine.css";
 const HistoryLine = (props) => {
   return (
-    <p className="history-line">
-      {props.children} <FontAwesomeIcon icon={faCheck} />
+    <p title={props.checked && "checked by admin."}>
+      <Link
+        className={`history-line ${props.checked ? "checked" : ""}`}
+        to={"/downloads/" + props.id}
+      >
+        {props.children} {props.checked && <FontAwesomeIcon icon={faCheck} />}
+      </Link>
     </p>
   );
 };
