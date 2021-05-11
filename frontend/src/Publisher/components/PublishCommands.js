e116fd34c35aeabd548d32501eb4e3dc1a2b3a12
MD Shimul <mdshemul480@gmail.com>
2021-05-11 23:16:59 +0600

    adding publish command edit and delete ui

---
+++ frontend/src/Publisher/components/PublishCommands.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+const PublishCommands = (props) => {
+  return <div className="all-publish-commands"></div>;
+};
+
+export default PublishCommands;
