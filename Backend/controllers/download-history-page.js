01f1e47aa603d4421fe890e820ddd0b09ea2053b
MD Shimul <mdshemul480@gmail.com>
2021-05-15 22:07:51 +0600

    creating downloads history page routes

--- Backend/controllers/download-history-page.js
+++ Backend/controllers/download-history-page.js
@@ -10,4 +10,15 @@ const downloadHistoryAllDate = async (req, res, next) => {
   return res.json(allDates);
 };
 
+const dateAllMovies = async (req, res, next) => {
+  const { dateId } = req.params;
+  let allDates;
+  try {
+    allDates = await DownloadDate.findOne({ _id: dateId }).populate("movies");
+  } catch (err) {
+    return res.status(500).json({});
+  }
+  res.send(allDates);
+};
 exports.downloadHistoryAllDate = downloadHistoryAllDate;
+exports.dateAllMovies = dateAllMovies;
