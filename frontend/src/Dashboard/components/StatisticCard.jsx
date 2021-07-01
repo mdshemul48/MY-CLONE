dae0706c3d93bd6ef2419139beba3f392092fc8e
MD Shimul <mdshemul480@gmail.com>
2021-07-01 16:59:08 +0600

    adding style to statistic card

--- frontend/src/Dashboard/components/StatisticCard.jsx
+++ frontend/src/Dashboard/components/StatisticCard.jsx
@@ -7,17 +7,17 @@ import "./StatisticCard.css";
 const StatisticCard = () => {
   return (
     <Container
-      className="d-flex justify-content-between align-items-center w-100 mt-2 pt-3 Statistic-Card text-white"
+      className="d-flex justify-content-between align-items-center w-100 mt-2 pt-3 pb-1 Statistic-Card text-white"
       style={{ backgroundColor: "#29314F" }}
     >
-      <div>
-        <h5>50</h5>
+      <div className="info">
+        <h5 className="counter">50</h5>
         <p>
           <small>Tody Download</small>
         </p>
       </div>
       <div>
-        <FontAwesomeIcon icon={faDownload} />
+        <FontAwesomeIcon icon={faDownload} className="statistic__icon" />
       </div>
     </Container>
   );
