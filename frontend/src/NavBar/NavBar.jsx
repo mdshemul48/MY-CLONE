505a37e51a4d14ec4ece01b55140cd5a5f9ba8c1
MD Shimul <mdshemul480@gmail.com>
2021-07-01 13:39:18 +0600

    ending the nav bar setup

--- frontend/src/NavBar/NavBar.jsx
+++ frontend/src/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
@@ -8,15 +10,30 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#">Dashboard</Nav.Link>
-          <Nav.Link href="#">Movie Downloads</Nav.Link>
-          <Nav.Link href="#">Movie Publisher</Nav.Link>
-          <Nav.Link href="#">Create User</Nav.Link>
+          <Nav.Link href="#" className="navbar__navLink">
+            Dashboard
+          </Nav.Link>
+          <Nav.Link href="#" className="navbar__navLink">
+            Movie Downloads
+          </Nav.Link>
+          <Nav.Link href="#" className="navbar__navLink">
+            Movie Publisher
+          </Nav.Link>
+          <Nav.Link href="#" className="navbar__navLink">
+            Create User
+          </Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form>
+        <Nav>
+          <Nav.Link className="ml-2 mr-2 navbar__navLink  d-flex align-items-center">
+            {" "}
+            <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+            Logout
+          </Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
