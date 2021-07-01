82a22778b91c7585914c1e6139fe64a3f6787f9a
MD Shimul <mdshemul480@gmail.com>
2021-07-01 15:15:54 +0600

    setting the routes

--- frontend/src/NavBar/component/AllNavLink.jsx
+++ frontend/src/NavBar/component/AllNavLink.jsx
@@ -5,10 +5,10 @@ import NavLink from "./NavLink";
 const AllNavLink = () => {
   return (
     <Nav className="mr-auto">
-      <NavLink link="#" label="Dashboard" />
-      <NavLink link="#" label="Movie Downloads" />
-      <NavLink link="#" label="Movie Publisher" />
-      <NavLink link="#" label="Create User" />
+      <NavLink link="/" label="Dashboard" />
+      <NavLink link="/downloads" label="Movie Downloads" />
+      <NavLink link="/publish" label="Movie Publisher" />
+      <NavLink link="/user" label="Create User" />
     </Nav>
   );
 };
