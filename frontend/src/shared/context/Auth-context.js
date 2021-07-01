bfb84815f85e4b9c585f066a43fc0a72071cacc1
MD Shimul <mdshemul480@gmail.com>
2021-07-01 12:25:21 +0600

    removing frontend all files

--- frontend/src/shared/context/Auth-context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const AuthContext = createContext({
-  isLogin: false,
-  userId: null,
-  token: null,
-  login: () => {},
-  logout: () => {},
-});
