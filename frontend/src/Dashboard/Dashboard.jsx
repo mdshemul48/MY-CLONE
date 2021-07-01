1d439943a880e4f9ff0f6ff4beaf357a024a1c67
MD Shimul <mdshemul480@gmail.com>
2021-07-01 17:37:54 +0600

    adding new layout

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import StatisticCards from "./components/StatisticCards";
 
 const Dashboard = () => {
   return (
-    <Container>
-      <StatisticCards />
+    <Container fluid className="mt-3">
+      <Row>
+        <Col lg="8">
+          <Container>
+            <StatisticCards />
+          </Container>
+        </Col>
+        <Col lg="4"></Col>
+      </Row>
     </Container>
   );
 };
