51a258dbe0f2cd7c260f30dfdd24871628fcbb11
MD Shimul <mdshemul480@gmail.com>
2021-07-04 15:59:11 +0600

    removing table header nad adding border bottom 1px

--- frontend/src/MovieDownloads/MovieDownloads.jsx
+++ frontend/src/MovieDownloads/MovieDownloads.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Container, Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
+import "./MovieDownloads.css";
 const MovieDownloads = () => {
-  return <div>MovieDownloads</div>;
+  return (
+    <Container className="mt-5 bg-light">
+      <Table>
+        <thead>
+          <tr>
+            <th className="table-header">
+              Date <FontAwesomeIcon icon={faCaretDown} />{" "}
+            </th>
+            <th className="table-header">
+              Total Downloads <FontAwesomeIcon icon={faCaretDown} />
+            </th>
+            <th className="table-header">
+              Status <FontAwesomeIcon icon={faCaretDown} />
+            </th>
+          </tr>
+        </thead>
+      </Table>
+    </Container>
+  );
 };
 
 export default MovieDownloads;
