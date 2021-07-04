ee14b4da92ed987bfb1617c224cd253e35e2142d
MD Shimul <mdshemul480@gmail.com>
2021-07-04 16:07:49 +0600

    heading area done

--- frontend/src/MovieDownloads/MovieDownloads.jsx
+++ frontend/src/MovieDownloads/MovieDownloads.jsx
@@ -6,18 +6,19 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import "./MovieDownloads.css";
 const MovieDownloads = () => {
   return (
-    <Container className="mt-5 bg-light">
+    <Container className="mt-5 bg-light download-container">
       <Table>
         <thead>
-          <tr>
+          <tr className="">
             <th className="table-header">
-              Date <FontAwesomeIcon icon={faCaretDown} />{" "}
+              Date <FontAwesomeIcon className="ml-2" icon={faCaretDown} />{" "}
             </th>
             <th className="table-header">
-              Total Downloads <FontAwesomeIcon icon={faCaretDown} />
+              Total Downloads{" "}
+              <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
             </th>
             <th className="table-header">
-              Status <FontAwesomeIcon icon={faCaretDown} />
+              Status <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
             </th>
           </tr>
         </thead>
