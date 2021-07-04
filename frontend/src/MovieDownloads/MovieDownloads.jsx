d657cb9b6dbd974bd8930ec21ce2d140d8726773
MD Shimul <mdshemul480@gmail.com>
2021-07-04 16:31:08 +0600

    adding downloads history done

--- frontend/src/MovieDownloads/MovieDownloads.jsx
+++ frontend/src/MovieDownloads/MovieDownloads.jsx
@@ -8,6 +8,32 @@ const MovieDownloads = () => {
     <Container className="mt-5 bg-light download-container">
       <Table>
         <TableHeading />
+        <tbody>
+          <tr>
+            <td>04-07-2021</td>
+            <td>28 Movies</td>
+            <td>
+              {" "}
+              <span className="table-status-text">pending</span>{" "}
+            </td>
+          </tr>
+          <tr>
+            <td>04-07-2021</td>
+            <td>28 Movies</td>
+            <td>
+              {" "}
+              <span className="table-status-text verified">verified</span>{" "}
+            </td>
+          </tr>
+          <tr>
+            <td>04-07-2021</td>
+            <td>28 Movies</td>
+            <td>
+              {" "}
+              <span className="table-status-text">pending</span>{" "}
+            </td>
+          </tr>
+        </tbody>
       </Table>
     </Container>
   );
