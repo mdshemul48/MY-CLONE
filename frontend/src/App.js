481b3dc91e923cc9d4ecd2601212f3ab12c80565
MD Shimul <mdshemul480@gmail.com>
2021-07-07 20:54:28 +0600

    adding login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-
-import NavBar from "./NavBar/NavBar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+
+// import NavBar from "./NavBar/NavBar"
 import dashboard from "./Dashboard/Dashboard"
 import MovieDownloads from "./MovieDownloads/MovieDownloads"
 import MoviePublish from "./MoviePublish/MoviePublish"
 import CreateAccount from "./CreateAccount/CreateAccount"
 import DayDownloads from "./MovieDownloads/DayDownloads"
 import Login from "./Login/Login"
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
-      <NavBar />
+      {/* <NavBar /> */}
       <Switch>
         <Route path="/" exact component={dashboard} />
         <Route path="/downloads/:dayId" exact component={DayDownloads} />
--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+
+import "./Login.css";
 const Login = () => {
-  return <Container>login</Container>;
+  return (
+    <div className="d-flex justify-content-center align-items-center login-full-container">
+      <form className="">
+        <h4 className="mb-4 text-center Login-text">LOGIN</h4>
+        <input
+          className="form-control mb-3 p-4"
+          type="text"
+          Placeholder="Username"
+        />
+        <input
+          className="form-control mb-3 p-4"
+          type="password"
+          Placeholder="Password"
+        />
+        <button className="btn btn-dark btn-block">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
