6ec98e3846b2ce10c9d228fd38207fed39f69400
MD Shimul <mdshemul480@gmail.com>
2021-07-07 19:18:36 +0600

    adding link icon and link place

--- frontend/src/MoviePublish/components/PublishCommands.jsx
+++ frontend/src/MoviePublish/components/PublishCommands.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Card, InputGroup, FormControl } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFolder } from "@fortawesome/free-solid-svg-icons";
+import { faFolder, faLink } from "@fortawesome/free-solid-svg-icons";
 const PublishCommands = () => {
   return (
     <Container className="mt-5">
@@ -19,10 +19,19 @@ const PublishCommands = () => {
               </InputGroup.Text>
               <FormControl aria-describedby="basic-addon1" />
             </InputGroup>
+
             <InputGroup className="mb-3">
               <InputGroup.Text id="basic-addon1">
                 <FontAwesomeIcon icon={faFolder} />
                 <span className="ml-2">To</span>
+              </InputGroup.Text>
+              <FormControl aria-describedby="basic-addon1" />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">
+                <FontAwesomeIcon icon={faLink} />
+                <span className="ml-2">Link</span>
               </InputGroup.Text>
               <FormControl aria-describedby="basic-addon1" />
             </InputGroup>
