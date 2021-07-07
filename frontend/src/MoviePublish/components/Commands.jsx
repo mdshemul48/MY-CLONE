398edfe5c7d0332f1ca90660793eb7c32034b3c1
MD Shimul <mdshemul480@gmail.com>
2021-07-07 19:35:40 +0600

    adding props and value to the input box

--- frontend/src/MoviePublish/components/Commands.jsx
+++ frontend/src/MoviePublish/components/Commands.jsx
@@ -8,18 +8,19 @@ import {
   faFolderMinus,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Commands = () => {
+const Commands = (props) => {
+  const { name, from, to, link, catagory } = props;
   return (
     <div className="pt-3">
       <Card>
-        <Card.Header as="h5">Swedish movie</Card.Header>
+        <Card.Header as="h5">{name}</Card.Header>
         <Card.Body>
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1">
               <FontAwesomeIcon icon={faFolder} />
               <span className="ml-2">From</span>
             </InputGroup.Text>
-            <FormControl aria-describedby="basic-addon1" />
+            <FormControl aria-describedby="basic-addon1" value={from} />
           </InputGroup>
 
           <InputGroup className="mb-3">
@@ -27,7 +28,7 @@ const Commands = () => {
               <FontAwesomeIcon icon={faFolder} />
               <span className="ml-2">To</span>
             </InputGroup.Text>
-            <FormControl aria-describedby="basic-addon1" />
+            <FormControl aria-describedby="basic-addon1" value={to} />
           </InputGroup>
 
           <InputGroup className="mb-3">
@@ -35,7 +36,7 @@ const Commands = () => {
               <FontAwesomeIcon icon={faLink} />
               <span className="ml-2">Link</span>
             </InputGroup.Text>
-            <FormControl aria-describedby="basic-addon1" />
+            <FormControl aria-describedby="basic-addon1" value={link} />
           </InputGroup>
 
           <InputGroup className="mb-3">
@@ -43,7 +44,7 @@ const Commands = () => {
               <FontAwesomeIcon icon={faFolderMinus} />
               <span className="ml-2">Catagory</span>
             </InputGroup.Text>
-            <FormControl aria-describedby="basic-addon1" />
+            <FormControl aria-describedby="basic-addon1" value={catagory} />
           </InputGroup>
         </Card.Body>
       </Card>
