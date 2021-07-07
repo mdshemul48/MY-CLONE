a333dafb718d8ccd0efbb153a6b85408ae6c9261
MD Shimul <mdshemul480@gmail.com>
2021-07-07 19:07:28 +0600

    adding all publish command area

--- frontend/src/MoviePublish/components/PublishCommands.jsx
+++ frontend/src/MoviePublish/components/PublishCommands.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Container, Card } from "react-bootstrap";
 const PublishCommands = () => {
-  return <div className="">gg</div>;
+  return (
+    <Container className="mt-5">
+      <h3 className="mt-2 mb-3">All Publish Commands</h3>
+      <hr />
+
+      <div>
+        <Card>
+          <Card.Header as="h5">Swedish movie</Card.Header>
+          <Card.Body></Card.Body>
+        </Card>
+      </div>
+    </Container>
+  );
 };
 
 export default PublishCommands;
