fd6df83172f7205bca16be87b3c7097379b239ed
MD Shimul <mdshemul480@gmail.com>
2021-07-08 14:35:01 +0600

    adding redux provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"
+
+import store from "./Store/index"
 import App from './App';
+
 
 import "./Index.css"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
