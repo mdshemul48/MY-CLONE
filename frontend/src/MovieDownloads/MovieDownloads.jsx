72151fcaff3b813cf84d1302b7185b2a64b0ab7f
MD Shimul <mdshemul480@gmail.com>
2021-07-08 14:01:03 +0600

    adding title in movie downloads

--- frontend/src/MovieDownloads/MovieDownloads.jsx
+++ frontend/src/MovieDownloads/MovieDownloads.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { Container, Table } from "react-bootstrap";
 
+import PageTitle from "../Title/PageTitle";
 import TableHeading from "./components/TableHeading";
 import TableElement from "./components/TableElement";
 import "./MovieDownloads.css";
 const MovieDownloads = () => {
   return (
-    <Container className="mt-5 bg-light download-container">
-      <Table responsive>
-        <TableHeading />
-        <tbody>
-          <TableElement />
-          <TableElement />
-          <TableElement />
-        </tbody>
-      </Table>
-    </Container>
+    <>
+      <PageTitle>Downloads History</PageTitle>
+      <Container className="mt-5 bg-light download-container">
+        <Table responsive>
+          <TableHeading />
+          <tbody>
+            <TableElement />
+            <TableElement />
+            <TableElement />
+          </tbody>
+        </Table>
+      </Container>
+    </>
   );
 };
 
