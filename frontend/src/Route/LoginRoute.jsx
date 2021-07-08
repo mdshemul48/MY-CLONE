282f4acffd7e6bbf8984a9247d880db6f9a5c781
MD Shimul <mdshemul480@gmail.com>
2021-07-08 16:41:26 +0600

    adding default route

---
+++ frontend/src/Route/LoginRoute.jsx
@@ -0,0 +1,12 @@
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+const LoginRoute = (props) => {
+  const { user } = useSelector((state) => state.auth);
+  return user ? (
+    <Redirect to="/login" />
+  ) : (
+    <Route path={props.path} component={props.component} exact />
+  );
+};
+
+export default LoginRoute;
