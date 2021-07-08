7906d980eab36bd816f24fd362148f0c2e0669c9
MD Shimul <mdshemul480@gmail.com>
2021-07-08 14:24:35 +0600

    adding page title to login page

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
+import PageTitle from "../Title/PageTitle";
 import "./Login.css";
 const Login = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center login-full-container">
-      <form className="">
-        <h4 className="mb-4 text-center Login-text">LOGIN</h4>
-        <input
-          className="form-control mb-3 p-4"
-          type="text"
-          Placeholder="Username"
-        />
-        <input
-          className="form-control mb-3 p-4"
-          type="password"
-          Placeholder="Password"
-        />
-        <button className="btn btn-dark btn-block">Login</button>
-      </form>
-    </div>
+    <>
+      <PageTitle>Login</PageTitle>
+      <div className="d-flex justify-content-center align-items-center login-full-container">
+        <form className="">
+          <h4 className="mb-4 text-center Login-text">LOGIN</h4>
+          <input
+            className="form-control mb-3 p-4"
+            type="text"
+            Placeholder="Username"
+          />
+          <input
+            className="form-control mb-3 p-4"
+            type="password"
+            Placeholder="Password"
+          />
+          <button className="btn btn-dark btn-block">Login</button>
+        </form>
+      </div>
+    </>
   );
 };
 
