c93eec3a7341614a47df7f08a7d3ad7fad656012
MD Shimul <mdshemul480@gmail.com>
2021-07-08 19:24:17 +0600

    adding chart app

--- frontend/src/Dashboard/components/ChartArea.jsx
+++ frontend/src/Dashboard/components/ChartArea.jsx
@@ -1,21 +1,26 @@
 import { Bar } from "react-chartjs-2";
 import { Container } from "react-bootstrap";
-
+import { useSelector } from "react-redux";
 import "./ChartArea.css";
 const ChartArea = () => {
+  const downloadDays = useSelector(
+    (state) => state.dashboard.data.botWorkingData
+  );
+  const reversedData = [].concat(downloadDays).reverse();
   const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: reversedData.map((day) => day.date),
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "downloads",
+        data: reversedData.map((day) => day.download),
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
           "rgba(54, 162, 235, 0.2)",
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
@@ -23,7 +28,8 @@ const ChartArea = () => {
           "rgba(255, 206, 86, 1)",
           "rgba(75, 192, 192, 1)",
           "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(255, 99, 132, 1)",
         ],
         borderWidth: 1,
       },
