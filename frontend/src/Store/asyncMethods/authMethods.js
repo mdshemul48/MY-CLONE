619f52f68d5250bd585afc35f3938ac0e53d3cce
MD Shimul <mdshemul480@gmail.com>
2021-07-08 15:32:32 +0600

    adding login async methods

--- frontend/src/Store/asyncMethods/authMethods.js
+++ frontend/src/Store/asyncMethods/authMethods.js
@@ -3,6 +3,16 @@ import axios from "../../util/axiosConfig"
 
 export const loginMethods = (loginInfo) => {
     return async (dispatch) => {
-
+        try {
+            const response = await axios.post("/auth/login", loginInfo, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            console.log(response)
+        } catch (error) {
+            console.log(error.response.data.message)
+        }
     }
 }
