4bec5cf8dfcdae68066f0b16b44cca29007a7edd
MD Shimul <mdshemul480@gmail.com>
2021-07-11 22:34:53 +0600

    adding time to the bot failure area

--- frontend/src/Dashboard/components/BotFailureReport.jsx
+++ frontend/src/Dashboard/components/BotFailureReport.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import dateFormat from "dateformat";
 import { useSelector } from "react-redux";
 import { Container, Tabs, Tab } from "react-bootstrap";
 const BotFailureReport = () => {
@@ -10,21 +11,21 @@ const BotFailureReport = () => {
         <Tab eventKey="PUBLISH_BOT" title="PUBLISH BOT">
           {fails.publisherError.map((data) => (
             <p key={data._id} className="m-2 p-1 text-break">
-              {data.errorText}
+              [{dateFormat(data.time, "h:MM:ss TT d/mm/yy")}] {data.errorText}
             </p>
           ))}
         </Tab>
         <Tab eventKey="DOWNLOAD_BOT" title="DOWNLOAD BOT">
           {fails.downloaderError.map((data) => (
             <p key={data._id} className="m-2 p-1 text-break">
-              {data.errorText}
+              [{dateFormat(data.time, "h:MM:ss TT d/mm/yy")}] {data.errorText}
             </p>
           ))}
         </Tab>
         <Tab eventKey="UPLOAD_BOT" title="UPLOAD BOT">
           {fails.uploaderError.map((data) => (
             <p key={data._id} className="m-2 p-1 text-break text-break">
-              {data.errorText}
+              [{dateFormat(data.time, "h:MM:ss TT d/mm/yy")}] {data.errorText}
             </p>
           ))}
         </Tab>
