751b536be3eb112a59cbaa531ad356f1ef1823d4
MD Shimul <mdshemul480@gmail.com>
2021-07-16 16:35:06 +0600

    adding fetching function to eh web

--- frontend/src/MovieDownloads/DayDownloads.jsx
+++ frontend/src/MovieDownloads/DayDownloads.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { Container, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
@@ -7,29 +7,24 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import PageTitle from "../Title/PageTitle";
 import MovieCard from "./components/MovieCard";
 
-import axios from "../util/axiosConfig"
+import axios from "../util/axiosConfig";
 
 import "./DayDownloads.css";
 const DayDownloads = () => {
-  const [moviesData, setMoviesData] = useState({})
-  const { dayId } = useParams()
+  const [moviesData, setMoviesData] = useState({});
+  const { dayId } = useParams();
 
   useEffect(() => {
-    const fetchMovies = async() => {
-        try{
-          const response = await axios.get(`/download-page/${dayId}`)
-
-          setMoviesData(response.data)
-
-
-        }catch(err){
-          console.log(err)
-        }
-    }  
-    fetchMovies()
-  },[dayId])
-
-
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(`/download-page/${dayId}`);
+        setMoviesData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchMovies();
+  }, [dayId]);
 
   return (
     <>
@@ -40,7 +35,8 @@ const DayDownloads = () => {
           className="d-flex justify-content-between check-area pb-2"
         >
           <h4 className="text-break">
-            Working History Of {moviesData.date} ({moviesData?.movies?.length|| 0})
+            Working History Of {moviesData.date} (
+            {moviesData?.movies?.length || 0})
           </h4>
           <Button variant="success" className="checkButton">
             verify
@@ -50,9 +46,8 @@ const DayDownloads = () => {
 
         <Container fluid>
           {moviesData?.movies?.map((movie) => {
-              return <MovieCard key={movie._id} movie={movie} />
+            return <MovieCard key={movie._id} movie={movie} />;
           })}
-          
         </Container>
       </Container>
     </>
